2a2bd7d4ba9fb0ad46e65e090906d851e55cd7b8
ranaasad43 <asadrana2@gmail.com>
2020-02-27 23:59:04 +0500

    passing array or state through props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import Ninjas from './Ninjas';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Pro App</h1>
-      <Ninjas name="Asad" age="31" belt="green"/>
-      <br/>
-      <Ninjas name="Ahsan" age="31" belt="red"/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ninjas: [
+      {name: 'Asad',age:31,belt:'green',id:1},
+      {name: 'Ahsan',age:31,belt:'red',id:2},
+      {name: 'Gadda',age:31,belt:'black',id:3}
+    ]
+  } 
+  render(){
+    return (
+      <div className="App">
+        <h1>Pro App</h1>
+        <Ninjas ninjas={this.state.ninjas}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
